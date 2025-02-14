b63851e6bbd7c79285de0b9fa5781ff1a889e8a4
Abinesh <abineshpcm@gmail.com>
2025-02-14 12:57:51 +0530

    if condition

---
+++ control statement and loops/conditional.py
@@ -0,0 +1,7 @@
+price = 1000000
+credit=input("How is the credit (good / bad):").lower()
+if credit == "good" :
+    downPayment = 0.1 * price
+else:
+    downPayment = 0.2 * price
+print(f"The Downpayment of {credit} is {downPayment}")

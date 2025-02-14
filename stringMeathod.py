b841b086cb79fb4292d575545f99e4ca365e7e62
Abinesh <abineshpcm@gmail.com>
2025-02-14 12:04:23 +0530

    stringMeathod

---
+++ stringMeathod.py
@@ -0,0 +1,8 @@
+course ="Python from scratch"
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.capitalize())
+print(course.find('r'))
+print(course.replace("scratch","Scratch to Advanced"))
+print("P" in course)

51bb7a2b66e90861b3ec35587c84cb22412228f8
Abinesh <abineshpcm@gmail.com>
2025-02-16 21:35:28 +0530

    for loop

---
+++ control statement and loops/forloopdict.py
@@ -0,0 +1,11 @@
+students = {
+    "Alice": 82,
+    "Bob": 67,
+    "Charlie": 90,
+    "David": 74,
+    "Eve": 88,
+    "Frank": 59
+}
+for name,Mark in students.items():
+    if (Mark > 75):
+        print(name)

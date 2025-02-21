88e6a41af9fe070b51049ee64da16fe4a1b8b546
Abinesh <abineshpcm@gmail.com>
2025-02-21 19:44:04 +0530

    Review

---
+++ Review/swap.py
@@ -0,0 +1,5 @@
+a = int(input("Enter a number :"))
+b = int(input("Enter second number"))
+print(f"The numbers before swap is a = {a} and b ={b}")
+a,b = b,a
+print(f"The numbers after swap is a={a} and b={b}")

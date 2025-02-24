827ae47ab321f589aff1109f4937aebd5e2fa047
Abinesh <abineshpcm@gmail.com>
2025-02-24 19:56:14 +0530

    list

---
+++ Data Structures/nestedlist/countEO.py
@@ -0,0 +1,9 @@
+list2 = list(map(int,input("Enter the numbers with seperated by spaces ").split()))
+evenCount = 0
+oddCount = 0
+for i in list2:
+    if (i % 2 == 0):
+        evenCount += 1
+    if (i % 2 != 0):
+      oddCount += 1
+print(f"The even count is {evenCount} and odd count is {oddCount}")

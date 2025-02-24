3abae36f7feeacdc9c0553ca3c3b6df325616f05
Abinesh <abineshpcm@gmail.com>
2025-02-24 19:40:27 +0530

    list

---
+++ Data Structures/nestedlist/list.py
@@ -0,0 +1,8 @@
+list1 = list(map(int,input("Enter the numbers seperated by spaces: ").split()))
+total = 0 
+for i in list1 :
+    total += i
+print (total)
+
+# list2=list(map(int,input("Enter the numbers followed by spaces: ").split()))
+# print(sum(list2))

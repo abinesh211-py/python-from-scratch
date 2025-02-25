ebc723d13774a93bdd384fb21517fc4a70e71976
Abinesh <abineshpcm@gmail.com>
2025-02-25 10:36:25 +0530

    tuples

---
+++ Data Structures/tuples/tuple2.py
@@ -0,0 +1,7 @@
+data = ((1, 'Alice', 85), (2, 'Bob', 90), (3, 'Charlie', 78))
+names = [item[1] for item in data]
+no = [item[0] for item in data]
+mark =[item[2] for item in data]
+print(names) 
+print(no)
+print(mark)
